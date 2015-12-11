b1ee2e6a8cb9f7d05673b769de18e38147ee9959
Alexander Vyushkov <baguage@gmail.com>
2015-12-11 18:17:25 -0500

    fixed bug in fabfile

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,7 @@ def update():
     local('git pull')
     local('pip install -r requirements.txt')
     local('touch django.log')
-    sudo('chown $USER:apache django.log')
+    local('sudo chown $USER:apache django.log')
     local('chmod g+w django.log')
     local('python manage.py syncdb')
     local('python manage.py collectstatic --noinput')
